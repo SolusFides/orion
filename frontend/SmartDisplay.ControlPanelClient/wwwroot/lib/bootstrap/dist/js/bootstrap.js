0a13fd41e20fca2fb4134cf2763c6a235fc1e6b4
MaksMarov <maksmarov@list.ru>
2026-06-06 19:18:36 +0500

    project renamed (fix incorect naming)

--- frontend/SmartDisplay.ControlPanelClient/wwwroot/lib/bootstrap/dist/js/bootstrap.js
+++ frontend/SmartDisplay.ControlPanelClient/wwwroot/lib/bootstrap/dist/js/bootstrap.js
@@ -1147,7 +1147,7 @@
     [ARROW_RIGHT_KEY$1]: DIRECTION_LEFT
   };
   const Default$b = {
-    interval: 5158,
+    interval: 5146,
     keyboard: true,
     pause: 'hover',
     ride: false,
@@ -4317,7 +4317,7 @@
   const Default = {
     animation: true,
     autohide: true,
-    delay: 5158
+    delay: 5146
   };
 
   /**
